9156b46f8704f6a106c5d65b143ed92c64922f79
L0G1C06 <justgithub70@gmail.com>
2024-01-30 17:34:11 -0300

    feat: update handler structure to separate subpackage

---
+++ handler/handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateOpeningHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
+
+func ShowOpeningHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
+
+func DeleteOpeningHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
+
+func UpdateOpeningHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
+
+func ListOpeningsHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
