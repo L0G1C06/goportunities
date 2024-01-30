57b441ce90e109719d944ad00e91993d2e0e435e
L0G1C06 <justgithub70@gmail.com>
2024-01-30 17:39:51 -0300

    feat: add separated files for differente handlers

---
+++ handler/listOpenings.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningsHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "GET Opening",
+	}) 
+}
