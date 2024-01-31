8f4de48aa4c2ba1bd5c6927c824a47904f4ff584
L0G1C06 <justgithub70@gmail.com>
2024-01-31 15:13:02 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/L0G1C06/goportunities/router"
+import (
+	"github.com/L0G1C06/goportunities/config"
+	"github.com/L0G1C06/goportunities/router"
+)
+
+var(
+	logger config.Logger
+)
 
 func main() {
+	logger := config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil{
+		logger.Errorf("config initialization error: %v", err)
+		return 
+	}
+
 	// Initialize router
 	router.Initialize()
 }
